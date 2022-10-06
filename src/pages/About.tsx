af247eb6c5079984bcae90894391b2dd6a9ab201
HTMLhead <mrdoo309@gmail.com>
2022-10-06 14:00:29 +0900

    :sparkles: about페이지 제작

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import { Link } from "../components";
+
+function About() {
+  return (
+    <div>
+      <div>about</div>
+      <Link href="/">
+        <button>go to main</button>
+      </Link>
+    </div>
+  );
+}
+
+export default About;
